ab4a4bf07890bc66f80b706b622f3a6104efb883
vpulkit007 <pulkit.0092013@gmail.com>
2021-07-19 16:47:48 +0530

    All file added

---
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -0,0 +1,17 @@
+import { LightningElement, track } from 'lwc';
+
+export default class HelloWorld extends LightningElement {
+    @track
+    inputVar = 'World';
+    @track
+    isChecked = false;
+
+    changeMethod(event)
+    {
+        this.inputVar = event.target.value;
+    }
+    changeMethod1(event)
+    {
+        this.isChecked = event.target.checked;
+    }
+}
